e6f7824fc25bab71b3623dc90a567403b4d9865e
Trychen <trychen@live.com>
2017-10-01 06:18:03 +0800

    Added some utils

--- src/main/java/cn/mccraft/pangu/core/util/NameBuilder.java
+++ src/main/java/cn/mccraft/pangu/core/util/NameBuilder.java
@@ -1,7 +1,11 @@
 package cn.mccraft.pangu.core.util;
 
 import javax.annotation.Nonnull;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Class used to build registry name
@@ -11,10 +15,12 @@ import java.util.Arrays;
 public interface NameBuilder {
     /**
      * Building the registry name, liking [hello, world] to hello_world
+     *
      * @param params all parts of name
      * @return built name
      */
-    @Nonnull static String buildRegistryName(String... params) {
+    @Nonnull
+    static String buildRegistryName(String... params) {
         StringBuilder stringBuilder = new StringBuilder(params[0]);
         String[] copied = Arrays.copyOfRange(params, 1, params.length);
         for (String s : copied) {
@@ -26,10 +32,12 @@ public interface NameBuilder {
 
     /**
      * Building the registry name, liking [hello, world] to HelloWorld
+     *
      * @param params all parts of name
      * @return built name
      */
-    @Nonnull static String buildUnlocalizedName(String... params) {
+    @Nonnull
+    static String buildUnlocalizedName(String... params) {
         StringBuilder stringBuilder = new StringBuilder(params[0]);
         String[] copied = Arrays.copyOfRange(params, 1, params.length);
         for (String s : copied) {
@@ -37,5 +45,30 @@ public interface NameBuilder {
             stringBuilder.append(s.substring(1));
         }
         return stringBuilder.toString();
+    }
+
+    static String[] apart(String name) {
+        return name.contains("_")? apartUnderline(name) : apartHump(name);
+    }
+
+    static String[] apartHump(String name) {
+        return humpToUnderline(name).split("_");
+    }
+
+    static String[] apartUnderline(String name) {
+        return name.split("_");
+    }
+
+    Pattern upperCasePattern = Pattern.compile("[A-Z]");
+
+        static String humpToUnderline(String str) {
+        Matcher matcher = upperCasePattern.matcher(str);
+        StringBuffer sb = new StringBuffer();
+        while (matcher.find()) {
+            matcher.appendReplacement(sb, "_" + matcher.group(0).toLowerCase());
+        }
+        matcher.appendTail(sb);
+        return sb.toString();
+
     }
 }
