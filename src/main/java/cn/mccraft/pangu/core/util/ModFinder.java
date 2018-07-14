6859518a0fabb5296d8d90d7731bbe08c3a6d68a
Trychen <trychen@live.com>
2018-07-14 16:57:21 +0800

    Fixed ModFinder from throwing NPE

--- src/main/java/cn/mccraft/pangu/core/util/ModFinder.java
+++ src/main/java/cn/mccraft/pangu/core/util/ModFinder.java
@@ -31,7 +31,7 @@ public interface ModFinder {
             return getModContainerFromClassByPackageName(target);
         }
 
-        if (source.getPath().endsWith(".class")) {
+        if (source == null || source.getPath().endsWith(".class")) {
             return getModContainerFromClassByPackageName(target);
         }
 
