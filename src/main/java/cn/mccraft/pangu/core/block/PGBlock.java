c15810de1869518e9fa67c408d15824cd5bb712c
Mouse <937279156@qq.com>
2018-07-24 21:56:29 +0800

    Update PGBlock

--- src/main/java/cn/mccraft/pangu/core/block/PGBlock.java
+++ src/main/java/cn/mccraft/pangu/core/block/PGBlock.java
@@ -1,6 +1,7 @@
 package cn.mccraft.pangu.core.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 
@@ -11,5 +12,16 @@ public class PGBlock extends Block {
 
     public PGBlock(Material material, MapColor mapColor) {
         super(material, mapColor);
+    }
+
+    @Override
+    public PGBlock setSoundType(SoundType sound) {
+        super.setSoundType(sound);
+        return this;
+    }
+
+    public PGBlock setHarvestLevelR(String toolClass, int level) {
+        super.setHarvestLevel(toolClass, level);
+        return this;
     }
 }
