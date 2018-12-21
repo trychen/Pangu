b1a1c293033b1243d8d5e2b01839eea4dba86eac
root <root@host.localdomain>
2018-12-21 02:11:47 -0500

    Updated UI

---
+++ src/main/java/cn/mccraft/pangu/core/client/ui/Button.java
@@ -0,0 +1,26 @@
+package cn.mccraft.pangu.core.client.ui;
+
+public abstract class Button extends Component {
+
+  public Button(int width, int height) {
+    super(width, height);
+  }
+
+  public abstract void onClick(int mouseButton, int mouseX, int mouseY);
+
+  @Override
+  public void onMousePressed(int mouseButton, int mouseX, int mouseY) {
+    if (actionOnReleased(mouseX, mouseY)) return;
+    this.onClick(mouseButton, mouseX, mouseY);
+  }
+
+  @Override
+  public void onMouseReleased(int mouseX, int mouseY) {
+    if (!actionOnReleased(mouseX, mouseY)) return;
+    this.onClick(0, mouseX, mouseY);
+  }
+
+  public boolean actionOnReleased(int mouseX, int mouseY) {
+    return true;
+  }
+}
--- src/main/java/cn/mccraft/pangu/core/client/ui/Container.java
+++ src/main/java/cn/mccraft/pangu/core/client/ui/Container.java
@@ -1,24 +1,84 @@
 package cn.mccraft.pangu.core.client.ui;
 
-import com.google.common.collect.Lists;
-import net.minecraft.client.Minecraft;
-
+import java.util.Collections;
 import javax.annotation.Nonnull;
-import java.util.List;
+import net.minecraft.util.NonNullList;
 
 public class Container extends Component {
-    public List<Container> containers = Lists.newArrayList();
+  private NonNullList<Component> components;
+  private Focusable focusedComponent;
 
-    /**
-     * Drawing this component
-     *
-     * @param minecraft
-     * @param mouseX
-     * @param mouseY
-     * @param partialTicks
-     */
-    @Override
-    public void draw(@Nonnull Minecraft minecraft, int mouseX, int mouseY, float partialTicks) {
+  public Container(int width, int height) {
+    super(width, height);
+  }
 
+  public Container addComponents(Component... cs) {
+    if (cs.length == 0) return this;
+    Collections.addAll(components, cs);
+    Collections.sort(components);
+
+    if (focusedComponent == null) {
+      for (Component c : cs) {
+        if (c instanceof Focusable) {
+          focus((Focusable) c);
+        }
+      }
     }
+
+    return this;
+  }
+
+  public Container addComponent(@Nonnull Component c) {
+    components.add(c);
+    Collections.sort(components);
+
+    if (focusedComponent == null && c instanceof Focusable) {
+      focus((Focusable) c);
+    }
+
+    return this;
+  }
+
+  public Container focus(@Nonnull Focusable c) {
+    if (focusedComponent != null) focusedComponent.onLostFocus();
+    focusedComponent = c;
+    focusedComponent.onFocused();
+    return this;
+  }
+
+  @Override
+  public void onDraw(float partialTicks, int mouseX, int mouseY) {
+    components.forEach(c -> c.onUpdata(mouseX, mouseY));
+    components
+        .stream()
+        .filter(Component::isVisable)
+        .forEach(c -> c.onDraw(partialTicks, mouseX, mouseY));
+  }
+
+  @Override
+  public void onMousePressed(int mouseButton, int mouseX, int mouseY) {
+    components
+        .stream()
+        .filter(Component::isHovered)
+        .forEach(
+            c -> {
+              if (c != focusedComponent && c instanceof Focusable) {
+                focus((Focusable) c);
+              }
+              c.onMousePressed(mouseButton, mouseX, mouseY);
+            });
+  }
+
+  @Override
+  public void onMouseReleased(int mouseX, int mouseY) {
+    components
+        .stream()
+        .filter(Component::isHovered)
+        .forEach(c -> c.onMouseReleased(mouseX, mouseY));
+  }
+
+  @Override
+  public void onKeyTyped(char typedChar, int keyCode) {
+    components.stream().filter(c -> !c.isDisabled()).forEach(c -> c.onKeyTyped(typedChar, keyCode));
+  }
 }
