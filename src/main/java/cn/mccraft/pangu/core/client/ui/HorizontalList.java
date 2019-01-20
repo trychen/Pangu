55e40df1eb4cd30b0bdb90acf36f77ed785733d9
Trychen <trychen@live.com>
2019-01-20 13:40:41 +0800

    Supported padding on HorizontalList

--- src/main/java/cn/mccraft/pangu/core/client/ui/HorizontalList.java
+++ src/main/java/cn/mccraft/pangu/core/client/ui/HorizontalList.java
@@ -4,6 +4,7 @@ import javax.annotation.Nonnull;
 
 public class HorizontalList extends Container {
     private float padding = 10;
+    private boolean centerPosition = true;
 
     public HorizontalList(int width, int height) {
         super(width, height);
@@ -33,7 +34,7 @@ public class HorizontalList extends Container {
     public void calPosition() {
         if (getComponents().size() == 0) return;
 
-        float wholeWidth = 0;
+        float wholeWidth = (getComponents().size() - 1) * padding;
         float wholeHeight = 0;
         for (Component component : getComponents()) {
             wholeWidth += component.getWidth();
@@ -41,7 +42,7 @@ public class HorizontalList extends Container {
         }
 
         float nowX = (width - wholeWidth) / 2;
-        float nowY = y + wholeHeight / 2;
+        float nowY = isCenterPosition()? y : y + wholeHeight / 2;
 
         for (Component component : getComponents()) {
             component.setPosition(nowX, nowY - component.getHeight() / 2);
@@ -55,6 +56,15 @@ public class HorizontalList extends Container {
 
     public HorizontalList setPadding(float padding) {
         this.padding = padding;
+        return this;
+    }
+
+    public boolean isCenterPosition() {
+        return centerPosition;
+    }
+
+    public HorizontalList setCenterPosition(boolean centerPosition) {
+        this.centerPosition = centerPosition;
         return this;
     }
 }
