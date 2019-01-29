910e64fdd4d2e32e06d2e588536000c0da1703d8
Trychen <trychen@live.com>
2019-01-29 10:15:04 +0800

    Using ByteDataStream:1.2 & Added 'of' method for TextureProvider

--- src/main/java/cn/mccraft/pangu/core/util/image/TextureProvider.java
+++ src/main/java/cn/mccraft/pangu/core/util/image/TextureProvider.java
@@ -2,6 +2,21 @@ package cn.mccraft.pangu.core.util.image;
 
 import net.minecraft.util.ResourceLocation;
 
+import java.net.MalformedURLException;
+
 public interface TextureProvider {
     ResourceLocation getTexture();
+
+    static TextureProvider of(String path, ResourceLocation missing) {
+        if (path.startsWith("http://") || path.startsWith("https://")) {
+            try {
+                return new RemoteImage(path, missing);
+            } catch (MalformedURLException e) {
+            }
+        } else if (path.startsWith("local:")) {
+            return new BuiltinImage(new ResourceLocation(path));
+        }
+
+        return null;
+    }
 }
