25127650ed588390fba64a8e1eddfdb7baf986a5
Trychen <trychen@live.com>
2019-02-15 20:39:08 +0800

    Better Scrolling List

--- src/main/java/cn/mccraft/pangu/core/client/ui/ScreenClothesChooser.java
+++ src/main/java/cn/mccraft/pangu/core/client/ui/ScreenClothesChooser.java
@@ -2,6 +2,7 @@ package cn.mccraft.pangu.core.client.ui;
 
 import cn.mccraft.pangu.core.client.input.BindKeyPress;
 import cn.mccraft.pangu.core.client.ui.builtin.EntityShow;
+import cn.mccraft.pangu.core.client.ui.builtin.Scrolling;
 import cn.mccraft.pangu.core.client.ui.builtin.ScrollingList;
 import cn.mccraft.pangu.core.client.ui.builtin.SelectionBox;
 import cn.mccraft.pangu.core.util.render.Rect;
@@ -27,16 +28,42 @@ public class ScreenClothesChooser extends Screen {
             return isSelected;
         }).setCenteredPosition(halfWidth - 120, halfHeight + 100));
 
-        ScrollingList scrollingList = new ScrollingList(200, 200);
-        scrollingList.add(100, (list, index, x, y, width) ->
-                Rect.drawGradientTop2Bottom(x + 1, y + 1, x + width - 1, y + 100 - 1, 0xFF7e21d3, 0xFFb886e9));
-        scrollingList.add(50, (list, index, x, y, width) ->
-                Rect.drawGradientTop2Bottom(x + 1, y + 1, x + width - 1, y + 50 - 1, 0xFF4ae290, 0xFF50c2e3));
+        GradientScrollingList scrollingList = new GradientScrollingList(50, 200);
         scrollingList.select(0);
-        addComponent(scrollingList.setPosition(width / 2, height / 2 - 100));
+        addComponent(scrollingList.setPosition(width / 2, height / 2 - 25));
     }
 
     @Override
     public void draw() {
+    }
+
+    class GradientScrollingList extends ScrollingList implements ScrollingList.Entry {
+        public GradientScrollingList(int height, int width) {
+            super(height, width);
+        }
+
+        @Override
+        public int getEntryHeight(ScrollingList list, int index) {
+            return index == 0?100:50;
+        }
+
+        @Override
+        public void onEntryDraw(ScrollingList list, int index, float x, float y, float width) {
+            if (index == 0) {
+                Rect.drawGradientTop2Bottom(x + 1, y + 1, x + width - 1, y + 100 - 1, 0xFF7e21d3, 0xFFb886e9);
+            } else {
+                Rect.drawGradientTop2Bottom(x + 1, y + 1, x + width - 1, y + 50 - 1, 0xFF4ae290, 0xFF50c2e3);
+            }
+        }
+
+        @Override
+        public int getEntryCounts() {
+            return 2;
+        }
+
+        @Override
+        public Entry getEntry(int index) {
+            return this;
+        }
     }
 }
--- src/main/java/cn/mccraft/pangu/core/client/ui/builtin/ScrollingList.java
+++ src/main/java/cn/mccraft/pangu/core/client/ui/builtin/ScrollingList.java
@@ -1,236 +1,117 @@
 package cn.mccraft.pangu.core.client.ui.builtin;
 
-import cn.mccraft.pangu.core.client.ui.Component;
 import cn.mccraft.pangu.core.util.render.Rect;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 import lombok.experimental.Delegate;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.ScaledResolution;
-import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import org.lwjgl.input.Mouse;
-import org.lwjgl.opengl.GL11;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ScrollingList extends Component {
-    public static final int SCROLL_BAR_WIDTH = 6;
-    protected float scrollFactor;
-    protected float initialMouseClickY = -2.0F;
-    @Getter
-    protected float scrollDistance;
-
-    @Getter
-    @Setter
-    protected float generalScrollingDistance = 8;
-
-    @Getter
-    @Setter
-    protected boolean highlightSelected = true;
-
+@Accessors(chain = true)
+public class ScrollingList extends Scrolling {
     @Delegate
     @Getter
     protected List<Entry> entries = new ArrayList<>();
 
     @Getter
-    protected int selectedIndex = -1;
+    @Setter
+    protected boolean highlightSelected = true;
 
     @Getter
-    @Setter
-    protected boolean showScrollBar = true;
+    protected int selectedIndex = -1;
 
     public ScrollingList(int height, int width) {
-        setSize(width, height);
-    }
-
-    public int getContentHeight() {
-        return entries.stream().mapToInt(it -> it.getEntryHeight()).sum();
-    }
-
-    public float getContentWidth() {
-        if (isShowScrollBar())
-            return getWidth() - SCROLL_BAR_WIDTH;
-        return getWidth();
+        super(height, width);
     }
 
     @Override
-    public void onDraw(float partialTicks, int mouseX, int mouseY) {
-
-        float scrollBarLeft = getX() + getContentWidth();
-        float scrollBarRight = scrollBarLeft + SCROLL_BAR_WIDTH;
-
-        if (Mouse.isButtonDown(0)) {
-            if (this.initialMouseClickY == -1.0F) {
-                if (isHovered()) {
-                    float mouseListY = mouseY - getY() + this.scrollDistance;
-                    float currentY = 0;
-                    for (int i = 0; i < getEntries().size(); i++) {
-                        Entry entry = getEntries().get(i);
-                        if (mouseListY >= currentY && mouseListY <= (currentY + entry.getEntryHeight())) {
-                            select(i);
-                        }
-                        currentY += entry.getEntryHeight();
-                    }
-//                    int slotIndex = mouseListY / this.slotHeight;
-//
-//                    if (mouseX >= entryLeft && mouseX <= entryRight && slotIndex >= 0 && mouseListY >= 0 && slotIndex < listLength) {
-//                        this.elementClicked(slotIndex, slotIndex == this.selectedIndex && System.currentTimeMillis() - this.lastClickTime < 250L);
-//                        this.selectedIndex = slotIndex;
-//                        this.lastClickTime = System.currentTimeMillis();
-//                    }
-
-                    if (mouseX >= scrollBarLeft && mouseX <= scrollBarRight) {
-                        this.scrollFactor = -1.0F;
-                        float scrollHeight = this.getContentHeight() - getHeight();
-                        if (scrollHeight < 1) scrollHeight = 1;
-
-                        float var13 = (getHeight() * getHeight()) / this.getContentHeight();
-
-                        if (var13 < 32) var13 = 32;
-                        if (var13 > getHeight()) var13 = getHeight();
-
-                        this.scrollFactor /= (getHeight() - var13) / (float) scrollHeight;
-                    } else {
-                        this.scrollFactor = 1.0F;
-                    }
-
-                    this.initialMouseClickY = mouseY;
-                } else {
-                    this.initialMouseClickY = -2.0F;
-                }
-            } else if (this.initialMouseClickY >= 0.0F) {
-                this.scrollDistance -= ((float) mouseY - this.initialMouseClickY) * this.scrollFactor;
-                this.initialMouseClickY = (float) mouseY;
+    public void onContentClick(float mouseListX, float mouseListY) {
+        float currentY = 0;
+        for (int i = 0; i < getEntryCounts(); i++) {
+            Entry entry = getEntry(i);
+            if (mouseListY >= currentY && mouseListY <= (currentY + entry.getEntryHeight(this, i))) {
+                select(i);
+                break;
             }
-        } else {
-            this.initialMouseClickY = -1.0F;
+            currentY += entry.getEntryHeight(this, i);
         }
+    }
 
-        applyScrollLimits();
-
-        Tessellator tess = Tessellator.getInstance();
-        BufferBuilder buffer = tess.getBuffer();
-
-        Minecraft client = Minecraft.getMinecraft();
-        ScaledResolution res = new ScaledResolution(client);
-
-        double scaleW = client.displayWidth / res.getScaledWidth_double();
-        double scaleH = client.displayHeight / res.getScaledHeight_double();
-
-        GL11.glEnable(GL11.GL_SCISSOR_TEST);
-        GL11.glScissor((int) (getX() * scaleW), (int) (client.displayHeight - ((getY() + getHeight()) * scaleH)),
-                (int) (getWidth() * scaleW), (int) (getHeight() * scaleH));
-
-        float baseY = this.getY() - this.scrollDistance;
-
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void onContentDraw(float baseY) {
         float slotTop = baseY;
-        for (int index = 0; index < entries.size(); index++) {
-            Entry entry = entries.get(index);
+        for (int index = 0; index < getEntryCounts(); index++) {
+            Entry entry = getEntry(index);
 
-            if (slotTop <= getY() + getHeight() && slotTop + entry.getEntryHeight() >= getY()) {
+            if (slotTop <= getY() + getHeight() && slotTop + entry.getEntryHeight(this, index) >= getY()) {
                 if (this.highlightSelected && this.isSelected(index)) {
-                    Rect.drawFrame(getX(), slotTop, getX() + getContentWidth(), slotTop + entry.getEntryHeight(), 1, 0xFF808080);
+                    Rect.drawFrame(getX(), slotTop, getX() + getContentWidth(), slotTop + entry.getEntryHeight(this, index), 1, 0xFF808080);
                 }
                 entry.onEntryDraw(this, index, getX(), slotTop, getContentWidth());
             }
 
-            slotTop += entry.getEntryHeight();
-        }
-
-        // Scrolling bar
-        float extraHeight = this.getContentHeight() - getHeight();
-
-        if (isShowScrollBar() && extraHeight > 0) {
-            float height = (getHeight() * getHeight()) / this.getContentHeight();
-
-            if (height > getHeight()) height = getHeight();
-
-            if (height < 32) height = 32;
-
-            float barTop = this.scrollDistance * (getHeight() - height) / extraHeight + getY();
-            if (barTop < getY()) barTop = getY();
-
-            GlStateManager.disableTexture2D();
-            buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_COLOR);
-            buffer.pos(scrollBarLeft, getY() + getHeight(), 0.0D).tex(0.0D, 1.0D).color(0x00, 0x00, 0x00, 0xFF).endVertex();
-            buffer.pos(scrollBarRight, getY() + getHeight(), 0.0D).tex(1.0D, 1.0D).color(0x00, 0x00, 0x00, 0xFF).endVertex();
-            buffer.pos(scrollBarRight, getY(), 0.0D).tex(1.0D, 0.0D).color(0x00, 0x00, 0x00, 0xFF).endVertex();
-            buffer.pos(scrollBarLeft, getY(), 0.0D).tex(0.0D, 0.0D).color(0x00, 0x00, 0x00, 0xFF).endVertex();
-            tess.draw();
-            buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_COLOR);
-            buffer.pos(scrollBarLeft, barTop + height, 0.0D).tex(0.0D, 1.0D).color(0x80, 0x80, 0x80, 0xFF).endVertex();
-            buffer.pos(scrollBarRight, barTop + height, 0.0D).tex(1.0D, 1.0D).color(0x80, 0x80, 0x80, 0xFF).endVertex();
-            buffer.pos(scrollBarRight, barTop, 0.0D).tex(1.0D, 0.0D).color(0x80, 0x80, 0x80, 0xFF).endVertex();
-            buffer.pos(scrollBarLeft, barTop, 0.0D).tex(0.0D, 0.0D).color(0x80, 0x80, 0x80, 0xFF).endVertex();
-            tess.draw();
-            buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_COLOR);
-            buffer.pos(scrollBarLeft, barTop + height - 1, 0.0D).tex(0.0D, 1.0D).color(0xC0, 0xC0, 0xC0, 0xFF).endVertex();
-            buffer.pos(scrollBarRight - 1, barTop + height - 1, 0.0D).tex(1.0D, 1.0D).color(0xC0, 0xC0, 0xC0, 0xFF).endVertex();
-            buffer.pos(scrollBarRight - 1, barTop, 0.0D).tex(1.0D, 0.0D).color(0xC0, 0xC0, 0xC0, 0xFF).endVertex();
-            buffer.pos(scrollBarLeft, barTop, 0.0D).tex(0.0D, 0.0D).color(0xC0, 0xC0, 0xC0, 0xFF).endVertex();
-            tess.draw();
-            GlStateManager.enableTexture2D();
-        }
-
-        GlStateManager.shadeModel(GL11.GL_FLAT);
-        GL11.glDisable(GL11.GL_SCISSOR_TEST);
-    }
-
-    private void applyScrollLimits() {
-        float listHeight = this.getContentHeight() - getHeight();
-
-        if (listHeight < 0) listHeight /= 2;
-
-        if (this.scrollDistance < 0.0F) this.scrollDistance = 0.0F;
-
-        if (this.scrollDistance > listHeight)
-            this.scrollDistance = listHeight;
-    }
-
-    @Override
-    public void onMouseInput(int mouseX, int mouseY) {
-        int scroll = Mouse.getEventDWheel();
-        if (scroll != 0) {
-            this.scrollDistance += (-1 * scroll / 120.0F) * this.generalScrollingDistance / 2;
+            slotTop += entry.getEntryHeight(this, index);
         }
     }
+
 
     public boolean isSelected(int index) {
         return index == getSelectedIndex();
     }
 
     public void select(int index) {
-        if (index >= 0 && index < getEntries().size())
-            this.selectedIndex = index;
+        if (isSelected(index)) return;
+        if (index < 0 || index >= getEntryCounts()) throw new IndexOutOfBoundsException();
+        if (!getEntry(index).onEntryStateChanged(this, index,true)) return;
+        for (int i = 0; i < getEntryCounts(); i++) {
+            if (i != index && !getEntry(i).onEntryStateChanged(this, i, i == index))
+                return;
+        }
+        this.selectedIndex = index;
     }
 
-    public ScrollingList add(int height, FunctionalEntry entry) {
-        add(new Entry() {
-            @Override
-            public int getEntryHeight() {
-                return height;
-            }
+    public int getContentHeight() {
+        int total = 0;
+        for (int i = 0; i < getEntryCounts(); i++) {
+            total += getEntry(i).getEntryHeight(this, i);
+        }
+        return total;
+    }
 
-            @Override
-            public void onEntryDraw(ScrollingList list, int index, float x, float y, float width) {
-                entry.onEntryDraw(list, index, x, y, width);
-            }
-        });
-        return this;
+    public int getEntryCounts() {
+        return getEntries().size();
+    }
+
+    public Entry getEntry(int index) {
+        return getEntries().get(index);
     }
 
     public interface Entry {
-        int getEntryHeight();
+        /**
+         * The height of entry
+         */
+        int getEntryHeight(ScrollingList list, int index);
 
+        /**
+         * @param list scrolling list
+         * @param index the index of this entry
+         * @param x entry left
+         * @param y entry top
+         * @param width the content width of the list
+         */
         void onEntryDraw(ScrollingList list, int index, float x, float y, float width);
-    }
 
-    @FunctionalInterface
-    public interface FunctionalEntry {
-        void onEntryDraw(ScrollingList list, int index, float x, float y, float width);
+        /**
+         * @param isSelected true if this entry selected
+         * @return if allow changing the index
+         */
+        default boolean onEntryStateChanged(ScrollingList list, int index, boolean isSelected) {
+            return true;
+        }
     }
 }
