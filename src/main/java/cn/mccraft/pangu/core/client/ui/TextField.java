eca2444bbf0a64bae0ce988871fe07c7533e5115
trychen <chenzhilins@qq.com>
2019-07-24 13:07:26 +0800

    Add text event

--- src/main/java/cn/mccraft/pangu/core/client/ui/TextField.java
+++ src/main/java/cn/mccraft/pangu/core/client/ui/TextField.java
@@ -5,7 +5,9 @@ import cn.mccraft.pangu.core.util.font.FontProvider;
 import cn.mccraft.pangu.core.util.render.Rect;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 import net.minecraft.client.gui.GuiScreen;
@@ -53,6 +55,10 @@ public class TextField extends Component implements Focusable {
     @Setter
     /** Called to check if the text is valid */
     protected Predicate<String> validator = Predicates.alwaysTrue();
+
+    @Getter
+    @Setter
+    protected TextChangeEvent textChangeEvent;
 
     @Getter
     @Setter
@@ -332,8 +338,10 @@ public class TextField extends Component implements Focusable {
         }
 
         if (this.validator.apply(s)) {
-            this.text = s;
-            this.moveCursorBy(i - this.selectionEnd + l);
+            if (textChangeEvent == null || textChangeEvent.onTextChange(s, this.text)) {
+                this.text = s;
+                this.moveCursorBy(i - this.selectionEnd + l);
+            }
         }
     }
 
@@ -503,5 +511,9 @@ public class TextField extends Component implements Focusable {
         }
 
         return i;
+    }
+    @FunctionalInterface
+    public interface TextChangeEvent {
+        boolean onTextChange(String newText, String oldText);
     }
 }
